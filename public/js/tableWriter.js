e59092e0501d2f18b1d9d936f3cb3a8d8396cf6a
David Jantz <dljantz@gmail.com>
2024-11-05 21:09:16 -0600

    Finished dynamic table generation for index.html

--- public/js/tableWriter.js
+++ public/js/tableWriter.js
@@ -1,9 +1,5 @@
 import data from './data.js';
 
-// Don't change anything above this comment
-
-
-// Feel free to define more functions if you want.
 
 
 /**
@@ -22,11 +18,33 @@ import data from './data.js';
  * @return HTML table element
  */
 function createTableElement(){
-    let tableElement = document.createElement('table');
-    // Add your code here to create the HTML table
+    const tableElement = document.createElement('table'); // create table
+    const thead = document.createElement('thead'); // create table header
+    const headerRow = document.createElement('tr'); // create header row
+    // populate with first row with titles from 2d array
+    for (let col=0; col<data[0].length; col++) {
+        const columnTitle = document.createElement('th');
+        columnTitle.textContent = data[0][col];
+        headerRow.appendChild(columnTitle);
+    }
 
+    thead.appendChild(headerRow);
+    tableElement.appendChild(thead);
 
-    // return the HTML table element
+    // create table body, then nested for loop to populate each cell in each row
+    //      loop traverses in row-major order of course
+    const tableBody = document.createElement('tbody');
+    for (let row=1; row<data.length; row++) {
+        const tableRow = document.createElement('tr');
+        for (let col=0; col<data[0].length; col++) {
+            const cell = document.createElement('td');
+            cell.textContent = data[row][col];
+            tableRow.appendChild(cell);
+        }
+        tableBody.appendChild(tableRow);
+    }
+    tableElement.appendChild(tableBody);
+
     return tableElement;
 }
 
