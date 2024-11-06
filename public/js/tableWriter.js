633c574e4a1135670f7598893b2512f2038ec679
David Jantz <dljantz@gmail.com>
2024-11-05 21:16:57 -0600

    A few small changes

--- public/js/tableWriter.js
+++ public/js/tableWriter.js
@@ -1,20 +1,6 @@
 import data from './data.js';
 
-
-
 /**
- * Create and return an HTML table element using the value of variable 'data.'
- * The value of data is an array in which each element in turn is an array.
- * The top level array can have any number of elements greater than 1.
- * Each nested array will have the same number of elements.
- * This number will always be greater tha 0.
- * The HTML element created by this function will have as many columns
- * as the length of each nested array.
- * The elements in the first nested array (i.e., the array at index 0 in "data")
- * will be used to create <th> elements in the <thead> element of the table
- * The values of all other nested arrays will be used to create rows in the table,
- * one row for each of the other nested arrays, and using the value in a
- * nested array to create <td> elements in that row of the table.
  * @return HTML table element
  */
 function createTableElement(){
@@ -48,7 +34,6 @@ function createTableElement(){
     return tableElement;
 }
 
-// Don't change anything below this comment
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('dynamicTable').appendChild(createTableElement())
 })
